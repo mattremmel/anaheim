580f8699005070cf4523d4f15069b3a1764428f8
Matthew Remmel <matt.remmel@gmail.com>
2023-08-27 14:50:07 -0400

    add comments

--- anaheim-derive/src/controller.rs
+++ anaheim-derive/src/controller.rs
@@ -21,18 +21,42 @@ fn expand_controller_struct(
 }
 
 fn expand_controller_impl(attr_args: TokenStream, mut item_impl: ItemImpl) -> Result<TokenStream> {
+    // TODO: These attr_args get mapped into OpenApi controller attribute
     let _attr_args = parse_attr_args(attr_args)?;
 
     for item in &mut item_impl.items {
+
+        // TODO: Each of these ImplItems is a Fn Route, and would have #[route] attribute
         if let ImplItem::Fn(method) = item {
             for attr in &mut method.attrs {
-                if attr.meta.
+                // TODO: Search for attribute named 'route'
+                // if attr.meta.
             }
         }
     }
 
+    // TODO: Need to individually put impl items in, so we can add #[oai] attributes
     Ok(quote! {
         #[::anaheim::web::OpenApi]
         #item_impl
     })
+}
+
+// TODO: These need parsed from syn_utils::Arg, but looks like Darling crate is better option
+struct ControllerParameters {
+    path: (),
+    method: (),
+    tag: (),
+}
+
+struct RouteParameters {
+    path: (),
+    method: (),
+    deprecated: (),
+    hidden: (),
+}
+
+struct RouteArgumentParameters {
+    name: (),
+    deprecated: (),
 }
